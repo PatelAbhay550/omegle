fbfe1599882c0b49916bfc07828e089541a90146
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2023-11-24 11:59:07 +0530

    bug fixes
    video play stop feature added

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const webcamElement = document.getElementById("webcam");
 const canvasElement = document.getElementById("canvas");
 const snapSoundElement = document.getElementById("snapSound");
+const stopButton = document.getElementById("stop");
+const stopButtond = document.getElementById("stopdes");
+
 const webcam = new Webcam(
   webcamElement,
   "user",
@@ -15,3 +18,21 @@ webcam
   .catch((err) => {
     console.log(err);
   });
+let isPlaying = true; // Track the current state (playing or stopped)
+
+function togglePlayStop() {
+  if (isPlaying) {
+    webcam.stop();
+    stopButton.textContent = "Play";
+    stopButtond.textContent = "Play";
+  } else {
+    webcam.start();
+    stopButton.textContent = "Stop";
+    stopButtond.textContent = "Stop";
+  }
+  isPlaying = !isPlaying; // Toggle the state
+}
+
+// Add event listener to the stop button
+stopButton.addEventListener("click", togglePlayStop);
+stopButtond.addEventListener("click", togglePlayStop);
